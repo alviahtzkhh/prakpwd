6439f2efec035f86a8033d6de518c391147e4fd9
alviahtzkhh <alviahtu05@gmail.com>
2021-12-24 19:26:25 +0700

    praktikum10

--- webservice/getdatamhs.php
+++ webservice/getdatamhs.php
@@ -3,7 +3,7 @@ require_once "koneksi.php";
 $sql = "select * from mahasiswa";
 $query = mysqli_query($con,$sql);
 while ($row = mysqli_fetch_assoc($query)) {
- $data[] = $row;
+    $data[] = $row;
 }
 header('content-type: application/json');
 echo json_encode($data);
