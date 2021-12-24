66d40f5a1842e5fcbf93d6cca8f4e0ee0079f715
alviahtzkhh <alviahtu05@gmail.com>
2021-12-24 19:27:50 +0700

    praktikum10

--- webservice/aksesmhs.php
+++ webservice/aksesmhs.php
@@ -4,12 +4,13 @@ $client = curl_init($url);
 curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec($client);
 $result = json_decode($response);
+
 foreach ($result as $r) {
- echo "<p>";
- echo "NIM : " . $r->nim . "<br />";
- echo "Nama : " . $r->nama . "<br />";
- echo "jenis kel : " . $r->jkel . "<br />";
- echo "Alamat : " . $r->alamat . "<br />";
- echo "Tgl Lahir : " . $r->ttl . "<br />";
- echo "</p>";
+    echo "<p>";
+    echo "NIM : " . $r->nim . "<br />";
+    echo "Nama : " . $r->nama . "<br />";
+    echo "jenis kel : " . $r->jkel . "<br />";
+    echo "Alamat : " . $r->alamat . "<br />";
+    echo "Tgl Lahir : " . $r->ttl . "<br />";
+    echo "</p>";
 }
--- webservice/koneksi.php
+++ webservice/koneksi.php
@@ -3,6 +3,7 @@ $host="localhost";
 $username="id18174215_pwd2021";
 $password="rBfBqv0er_9JH>Ic";
 $databasename="id18174215_akademik";
+
 $con=@mysqli_connect($host,$username,$password,$databasename);
 
 if (!$con) {
